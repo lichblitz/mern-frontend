e613dc427c59361e3540b339498c887b170b5614
lichblitz <carloscortespa@gmail.com>
2023-07-19 16:23:33 -0500

    refactor: Remove invalid config files

--- src/ui/config.js
+++
@@ -1,6 +0,0 @@
-import { providers } from "@data-provider/core";
-
-console.log(process.env.REACT_APP_BASE_URL);
-providers.getByTag("axios").config({
-  baseUrl: process.env.REACT_APP_BASE_URL,
-});
