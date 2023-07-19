a8a897c3eb76a9a521cec3779a5bf5cc05b2f745
lichblitz <carloscortespa@gmail.com>
2023-07-19 17:39:28 -0500

    refactor: Add new configuration files

--- README.md
+++ README.md
@@ -13,3 +13,44 @@ React application for managing Movies and Series hosted in a MongoDB.
 ## App details
 - Created with `npx create-react-app` and improved with `react-app-rewired` dependency.
 - Project architecture is an aproach to the Clean Architecture concepts.
+
+
+```json
+{
+    "compilerOptions": {
+        "target": "es6", // Target ECMAScript version (ES6 and above)
+        "lib": [
+            "dom",
+            "dom.iterable",
+            "esnext"
+        ], // List of library files to be included in the compilation
+        "jsx": "react", // Use React JSX for rendering
+        "module": "esnext", // Specify the module system (ES modules)
+        "esModuleInterop": true, // Enable compatibility with CommonJS imports
+        "strict": true, // Enable all strict type-checking options
+        "skipLibCheck": true, // Skip type checking of declaration files to improve performance
+        "forceConsistentCasingInFileNames": true, // Ensure consistent casing for file names
+        "noImplicitAny": true, // Disallow implicit 'any' types
+        "strictNullChecks": true, // Enable strict null checks
+        "strictFunctionTypes": true, // Enable strict checking of function types
+        "strictPropertyInitialization": true, // Enable strict property initialization checks
+        "noUnusedLocals": true, // Report errors on unused variables
+        "noUnusedParameters": true, // Report errors on unused function parameters
+        "noImplicitReturns": true, // Report error when not all code paths return a value
+        "noFallthroughCasesInSwitch": true, // Report errors for fallthrough cases in switch statements
+        "baseUrl": ".", // Base directory to resolve non-relative module names
+        "paths": {
+            "@/*": [
+                "src/*"
+            ] // Add aliases for absolute imports
+        }
+    },
+    "include": [
+        "src"
+    ], // List of files to include in the compilation
+    "exclude": [
+        "node_modules",
+        "build"
+    ] // List of files to exclude from the compilation
+}
+```
---
+++ webpack.config.js
@@ -0,0 +1,132 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+
+module.exports = function (_env, argv) {
+    const isProduction = argv.mode === "production";
+    const isDevelopment = !isProduction;
+
+    return {
+        mode: isProduction ? "production" : "development",
+        devtool: isDevelopment && "cheap-module-source-map",
+        entry: "./src/main.tsx",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "assets/js/[name].[contenthash:8].js",
+            publicPath: "/"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx|js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            cacheDirectory: true,
+                            cacheCompression: false,
+                            envName: isProduction ? "production" : "development"
+                        }
+                    }
+                },
+                {
+                    test: /\.(png|jpg|gif)$/i,
+                    use: {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192,
+                            name: "static/media/[name].[hash:8].[ext]"
+                        }
+                    }
+                },
+                {
+                    test: /\.svg$/,
+                    use: ["@svgr/webpack"]
+                },
+                {
+                    test: /\.(eot|otf|ttf|woff|woff2)$/,
+                    loader: require.resolve("file-loader"),
+                    options: {
+                        name: "static/media/[name].[hash:8].[ext]"
+                    }
+                }
+
+            ]
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                // Add other aliases if needed
+            },
+            extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        },
+        plugins: [
+            isProduction &&
+            new webpack.DefinePlugin({
+                "process.env.NODE_ENV": JSON.stringify(
+                    isProduction ? "production" : "development"
+                )
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "src/index.html"),
+                inject: true
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: "public", to: "assets" }
+                ],
+            }),
+            new Dotenv(),
+        ].filter(Boolean),
+        optimization: {
+            minimize: isProduction,
+            minimizer: [
+                new TerserWebpackPlugin({
+                    terserOptions: {
+                        compress: {
+                            comparisons: false
+                        },
+                        mangle: {
+                            safari10: true
+                        },
+                        output: {
+                            comments: false,
+                            ascii_only: true
+                        },
+                        warnings: false
+                    }
+                })
+            ],
+            splitChunks: {
+                chunks: "all",
+                minSize: 0,
+                maxInitialRequests: 20,
+                maxAsyncRequests: 20,
+                cacheGroups: {
+                    vendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name(module, chunks, cacheGroupKey) {
+                            const packageName = module.context.match(
+                                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+                            )[1];
+                            return `${cacheGroupKey}.${packageName.replace("@", "")}`;
+                        }
+                    },
+                    common: {
+                        minChunks: 2,
+                        priority: -10
+                    }
+                }
+            },
+            runtimeChunk: "single"
+        },
+        devServer: {
+            compress: true,
+            historyApiFallback: true,
+            open: true,
+        }
+    };
+};
